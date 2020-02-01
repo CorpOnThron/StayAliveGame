d3366e58f59c319e6773e727e25135c9bad2c41a
LeonWang <251280943@qq.com>
2020-02-01 13:53:06 -0500

    doctor scene  and maincontroller for timer

--- Assets/Scrips/Common/MainController.cs
+++ Assets/Scrips/Common/MainController.cs
@@ -17,28 +17,87 @@ public class MainController : MonoBehaviour
     Slider slider_temperature;
 
     public float pulseRate = 0.5f;
+    public float beatRate = 0.5f;
+    public Image heart;
+    public const float C_normalTemperature = 36.5f;
+    public const float C_normalTempSlider = 0.715f;
+    private float curTemperature;
+    Text txt_tempC;
+    Text txt_CD;
+
+
 
     void Start()
     {
-        slider_heartRate = main_Canvas.transform.Find("Slider_heartRate").GetComponent<Slider>();
-        slider_bloodPressure = main_Canvas.transform.Find("Slider_bloodPressure").GetComponent<Slider>();
+        InitAll();
+    }
+    void InitAll() {
+        curTemperature = C_normalTemperature;
+
+
+        txt_tempC = main_Canvas.transform.Find("Txt_Temp_C").GetComponent<Text>();
+        txt_CD = main_Canvas.transform.Find("Txt_Temp_CD").GetComponent<Text>();
         slider_temperature = main_Canvas.transform.Find("Slider_temperature").GetComponent<Slider>();
+
         SetupTimers();
     }
 
     void SetupTimers()
     {
-        timer_heartRate = new Timer(pulseRate, uint.MaxValue, () =>
-        {
-            iTween.PunchScale(slider_heartRate.handleRect.gameObject,iTween.Hash("x",2.6f,"y",2.6f,"easeType",iTween.EaseType.easeInOutBack,"time",0.5f));
-           
-        });
+        //heart rate timer
+        timer_heartRate = new Timer(pulseRate, uint.MaxValue, Pulse);
         TimersManager.AddTimers(this,new List<Timer>{timer_heartRate});
     }
 
-    void UpdateTimer() {
-        timer_heartRate.UpdateTimer();
+    #region heart rate
+    void Pulse() {
+        iTween.PunchScale(heart.gameObject, iTween.Hash("x", 1.05f, "y", 1.05f, "easeType", iTween.EaseType.easeInBack, "looptype",iTween.LoopType.loop, "time", beatRate));
     }
+
+    public void PulseRateUp() {
+        heart.transform.localScale = Vector3.one; 
+        beatRate -= 0.1f;
+       
+    }
+    public void PulseRateDown()
+    {
+        heart.transform.localScale = Vector3.one;
+        beatRate += 0.1f;
+    }
+    #endregion
+
+    #region temperature
+    // temperature
+    public void TempUp()
+    {
+        TemperatureBuffer(true,10);
+    }
+    public void TempDown()
+    {
+        TemperatureBuffer(false, 5);
+    }
+   
+    void TemperatureBuffer(bool isPositive,uint pSec) {
+        pSec *=10;
+        float tLen = curTemperature - C_normalTemperature;
+        float tStep = tLen / (float)pSec;
+        float tSliderValueStep = (slider_temperature.value - C_normalTempSlider)/(float)pSec;
+        float tSec = pSec;
+        timer_temperature = new Timer(0.1f,pSec,()=> {
+
+            slider_temperature.value = isPositive ? slider_temperature.value + tSliderValueStep : slider_temperature.value - tSliderValueStep;
+            curTemperature += tStep;
+            tSec -= 0.1f;
+            txt_CD.text = tSec + "s";
+            txt_tempC.text = curTemperature+ "°C";
+        });
+        TimersManager.AddTimers(this,new List<Timer> {timer_temperature });
+    }
+    public float GetTemperature() {
+        return curTemperature;
+    }
+    #endregion
+
 
     void Update()
     {
