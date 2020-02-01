9a45513e0a7327b4f21d1e72a6e260cbcdd3a822
Alex <koterator@gmail.com>
2020-02-01 15:36:24 -0500

    Enemy-1 done + animations

---
+++ Assets/Scripts/_Patients/BotController/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 3;
+    }
+
+    public void GetHit(int dmg) {
+        Health -=  dmg;
+        if (Health < 0) RestartGame();
+    
+    }
+
+    private void RestartGame() {
+        Debug.Log("I should die now");
+    }
+
+}
