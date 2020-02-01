bdfabbc6f21a020314844fa9057f90d2d4443640
Devesh Vasnani <dvasnani@my.centennialcollege.ca>
2020-02-01 15:01:53 -0500

    Added Photon Support and Created Basic UI

---
+++ Assets/Scrips/Network/PlayerInputField.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInputField : MonoBehaviour
+{
+
+    const string playerNamePrefKey = "PlayerName";
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.Log("Name cannot be null");
+            return;
+        }
+
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
