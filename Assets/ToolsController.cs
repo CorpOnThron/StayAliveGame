226554a4a8592d82a3cfab078660f4c9312b3883
LeonWang <251280943@qq.com>
2020-02-02 11:12:08 -0500

    medicine numbers left update

--- Assets/ToolsController.cs
+++ Assets/ToolsController.cs
@@ -12,16 +12,27 @@ public class ToolsController : MonoBehaviour
     public Vector3Int[] toolsFactors;
     public int[] toolsLeftNum;
     public FactorController factorCtrl;
-    
+    List<Text> toolsNum;
 
     void Start()
     {
         if (surgicalTools.Length == 0 || !factorCtrl) return;
+        toolsNum = new List<Text>();
+        for (int i = 0; i < surgicalTools.Length; i++)
+        {
+            toolsNum.Add( surgicalTools[i].transform.Find("Text").GetComponent<Text>());
+            toolsNum[i].text = toolsLeftNum[i].ToString();
+        }
+
     }
     public void OnClickTool(int id) {
         Debug.Log("Medicine : "+id + "; affects"+ toolsFactors[id]);
-        factorCtrl.GetMedicine(toolsFactors[id ]);
-
+        if (toolsLeftNum[id] > 0)
+        {
+            toolsLeftNum[id]--;
+            toolsNum[id].text = toolsLeftNum[id].ToString();
+            factorCtrl.GetMedicine(toolsFactors[id]);
+        }
     }
     void InitAll() {
       
